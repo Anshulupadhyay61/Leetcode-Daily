c070bdc12cca1b9016efcc3fb57eccfe3f4c3244
Anshulupadhyay61 <Anshulupadhyay544@gmail.com>
2026-07-23 12:20:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (98.31%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        bool isminus = false;
+        
+        long long int reverse = 0;
+        while(x != 0){
+            int digit = x % 10;
+
+            if (reverse > INT_MAX / 10)
+                return 0;
+            if (reverse < INT_MIN / 10)
+                return 0;
+
+
+            reverse = reverse * 10 + digit;
+            x /= 10; 
+        }
+        
+        return reverse;
+
+    }
+};
