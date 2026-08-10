f3d2b29dfccd6ff46f24e32ddd3ce0df5b80e6ac
Anshul Upadhyay <Anshulupadhyay544@gmail.com>
2026-08-11 01:14:04 +0530

    Time: 3 ms (53.28%), Space: 19.7 MB (53.83%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -4,7 +4,6 @@ class Solution:
         n=len(words)
         for i in range (len(words)):
             words[i]=words[i][::-1]
-            res = words[::-1]
         result= " ".join(words)
 
         return result
